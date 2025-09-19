02444ddc017201fb90b61816a62d0e0471117ab1
Harshita Sinha <204292568+Harshu52-ai@users.noreply.github.com>
2025-09-19 09:39:56 +0530

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <assert.h>
+#include "Test.h"
+#include "ColorUtils.h"
+
+using namespace TelCoColorCoder;
+
+void testNumberToPair(int pairNumber,
+    MajorColor expectedMajor,
+    MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+{
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
